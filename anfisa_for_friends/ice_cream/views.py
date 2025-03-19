c3928323fe7b0cc41fe72820c021afeae0a299c5
Sergei <114778342+underfmc@users.noreply.github.com>
2025-03-19 10:34:01 +0500

    Update views.py

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def ice_cream_list(request):
-    template = 'ice_cream/ice_cream_list.html'
+    template = 'ice_cream/list.html'
     return render(request, template)
 
 
 def ice_cream_detail(request, pk):
-    template = 'ice_cream/ice_cream_detail.html'
+    template = 'ice_cream/detail.html'
     return render(request, template)
